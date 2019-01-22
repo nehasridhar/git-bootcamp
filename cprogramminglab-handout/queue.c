64f61cbd848a4056ba72f9aa0268159141d72810
Neha Sridhar <nksridha@andrew.cmu.edu>
2019-01-22 13:44:25 -0500

    modified size function

--- cprogramminglab-handout/queue.c
+++ cprogramminglab-handout/queue.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Code for basic C skills diagnostic.
  * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
  * Modified to store strings, 2018
@@ -95,7 +95,8 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if (q == NULL) return 0;
+    else return 1;
 }
 
 /*
